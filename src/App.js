44976911a7ff5762471898f3362a551b85861788
allisoncortez <allizetorc@gmail.com>
2020-10-06 17:06:04 -0400

    initial Accordion component setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import Accordion from './components/Accordion'
 
 export default () => {
-    return <h1>Widgets App</h1>
+    return (
+        <div>
+            <Accordion />
+        </div>
+    )
 }
 
