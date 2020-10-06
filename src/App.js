5205a927588257ff8b838473975f9d4008c077ae
allisoncortez <allizetorc@gmail.com>
2020-10-06 17:12:35 -0400

    add item list to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import Accordion from './components/Accordion'
 
+const items = [
+    {
+        title: 'The What',
+        content: 'React is a front end JS framework'
+    },
+    {
+        title: 'The Why',
+        content: 'React is a popular JS library among engineers'
+    },
+    {
+        title: 'But How?',
+        content: 'You use React by creating components'
+    }
+]
+
 export default () => {
     return (
         <div>
