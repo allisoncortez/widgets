232126f6d0f3c74c403d792d5330f59ece609f5a
allisoncortez <allizetorc@gmail.com>
2020-10-15 18:20:39 -0400

    implement useState hook in Accordion.js, active variable updates classnames

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,13 +1,35 @@
-import React from 'react'
+// import React from 'react'
+import React, { useState } from 'react'
+//useState, a hook, gives us access to state inside of a component
+
 
 const Accordion = ({ items }) => {
-    const renderedItems = items.map(item => {
+    // initializes a new piece of state..
+    //left hand side: array destructuring
+    //activeIndex:the piece of state we are keeping track of(that will be changing/updating over time)
+    //setActiveIndex: a function we call to update our piece of state --> when we call this it causes our entire component to re-render
+    const [activeIndex, setActiveIndex] = useState(null);
+
+
+    const onTitleClick = (index) => {
+        // console.log('title clicked', index)
+
+        //update the value of our piece of state
+        setActiveIndex(index)
+        // same thing as saying the following: this.setState({ activeIndex: 10})
+    }
+
+    const renderedItems = items.map((item, index) => {
+        const active = index === activeIndex ? 'active' : '';
+
         return <React.Fragment key={item.title}>
-            <div className="title active">
+            <div className={`title ${active}`}
+                onClick={() => onTitleClick(index)}
+            >
                 <i className="dropdown icon"></i>
                 {item.title}
             </div>
-            <div className="content active">
+            <div className={`content ${active}`}>
                 <p>{item.content}</p>
             </div>
         </React.Fragment>
@@ -15,6 +37,6 @@ const Accordion = ({ items }) => {
     return <div className="ui styled accordion">
         {renderedItems}
     </div>
-}
+};
 
 export default Accordion
