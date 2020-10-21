040c1a7ebe76400c3a51f381fda1a8bff602af87
allisoncortez <allizetorc@gmail.com>
2020-10-21 11:34:41 -0400

    install axios, execute request from useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
     //create new piece of state
     const [term,setTerm] = useState('');
-    
+
+    //useEffect, similar to componentDidMount
+    //second argument tells when our code gets executed
+    //should be an empty array, array with something inside, or nothing at all
+    useEffect(() => {
+        const search = async () => {
+            await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                }
+            });
+        };
+
+        search();
+    }, [term]);
+
     return (
         <div>
             <div className="ui form">
