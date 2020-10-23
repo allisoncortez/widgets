ad2c2d508a056052a06966335f4a524fd5095c6c
allisoncortez <allizetorc@gmail.com>
2020-10-22 20:54:19 -0400

    render results in Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -27,6 +27,20 @@ const Search = () => {
         }
     }, [term]);
 
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }} ></span>
+             
+                </div>
+            </div>
+        )
+    })
+
     return (
         <div>
             <div className="ui form">
@@ -38,6 +52,7 @@ const Search = () => {
                     className="input" />
                 </div>
             </div>
+            <div className="ui celled list"> {renderedResults} </div>
         </div>
     )
 }
